4bd8f9153b3ceaff64047a1a5cc5493a611913fa
Leif <lak524@york.ac.uk>
2022-01-06 13:55:17 +0000

    Created libgdx project files

---
+++ desktop/src/io/github/annabeths/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package io.github.annabeths.desktop;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+import io.github.annabeths.eng1game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		new Lwjgl3Application(new eng1game(), config);
+	}
+}
