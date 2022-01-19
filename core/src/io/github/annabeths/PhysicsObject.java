7f9fadd580da0ccaf9b4a957513d3114d8a2a38d
AnnabethS <annabeths111@gmail.com>
2022-01-19 13:55:03 +0000

    implemented CollidesWith method

--- core/src/io/github/annabeths/PhysicsObject.java
+++ core/src/io/github/annabeths/PhysicsObject.java
@@ -1,6 +1,8 @@
 package io.github.annabeths;
 
 import com.badlogic.gdx.math.Polygon;
+import com.badlogic.gdx.utils.FloatArray;
+import com.badlogic.gdx.math.Intersector;
 
 public abstract class PhysicsObject implements GameObject {
     //in addition to the GameObject, PhysicsObjects must also implement the following
@@ -9,10 +11,11 @@ public abstract class PhysicsObject implements GameObject {
 
     boolean CollidesWith(PhysicsObject other)
     {
-        return false;
+        return Intersector.intersectPolygons(new FloatArray(collisionPolygon.getTransformedVertices()),
+                                             new FloatArray(other.collisionPolygon.getTransformedVertices()));
     }
     void OnCollision(PhysicsObject other)
     {
-        
+
     }
 }
