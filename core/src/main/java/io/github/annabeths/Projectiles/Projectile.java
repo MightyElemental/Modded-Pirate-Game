73550e37c48bc16a1e37248b7a646e508ce62ddb
James Burnell <94061765+uoy-jb2501@users.noreply.github.com>
2022-03-05 23:18:31 +0000

    Added limit to projectile travel
    Also:
    - removed repeated constructor code
    - added method to get speed
    - partially standardized center calculation
    - changed velocity calculation to use libGDX methods

--- core/src/main/java/io/github/annabeths/Projectiles/Projectile.java
+++ core/src/main/java/io/github/annabeths/Projectiles/Projectile.java
@@ -1,58 +1,69 @@
 package io.github.annabeths.Projectiles;
 
-import com.badlogic.gdx.graphics.g2d.*;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Polygon;
 import com.badlogic.gdx.math.Vector2;
 
 import io.github.annabeths.GameGenerics.PhysicsObject;
 
-import java.lang.Math;
-
+/**
+ * Projectiles are fired from other game objects. For example, cannonballs from
+ * ships or colleges.
+ * 
+ * @author Leif Kemp
+ * @author Annabeth Singleton
+ * @author James Burnell
+ */
 public class Projectile extends PhysicsObject {
 
 	private Vector2 velocity;
-	private float speed;
-	private Sprite sprite;
 	public boolean isPlayerProjectile;
 	public float damage;
+
+	/** How far the projectile can travel before dying */
+	public float lifeDist;
+	/**
+	 * lifeDist squared. Used to speed up distance calculations.
+	 * 
+	 * @see #lifeDist
+	 */
+	private float lifeDist2;
+	private Vector2 startingPos;
 
 	/**
 	 * @param origin where it should start
 	 * @param originRot the rotation of the projectile
 	 * @param data the data to use
 	 * @param isPlayerProjectile true if the projectile is shot by the player
+	 * @see #Projectile(Vector2, float, ProjectileData, boolean, float, float)
 	 */
 	public Projectile(Vector2 origin, float originRot, ProjectileData data,
 			boolean isPlayerProjectile) {
-		position = origin;
-		speed = data.speed;
-		this.isPlayerProjectile = isPlayerProjectile;
-		damage = data.damage;
-
-		// Calculate the projectile's velocity in the game space
-		velocity = new Vector2((float) Math.cos(Math.toRadians(originRot)) * speed,
-				(float) Math.sin(Math.toRadians(originRot)) * speed);
-
-		sprite = new Sprite(data.texture);
-		sprite.setSize(data.size.x, data.size.y);
-		sprite.setOrigin(data.size.x / 2, data.size.y / 2);
-		sprite.setRotation(originRot);
-
-		collisionPolygon = new Polygon(new float[] { data.size.x / 2, 0, data.size.x,
-				data.size.y / 2, data.size.x / 2, data.size.y, 0, data.size.y / 2 });
-		collisionPolygon.setOrigin(data.size.x / 2, data.size.y / 2);
+		this(origin, originRot, data, isPlayerProjectile, 1, 1);
 	}
 
+	/**
+	 * @param origin where it should start
+	 * @param originRot the rotation of the projectile
+	 * @param data the data to use
+	 * @param isPlayerProjectile true if the projectile is shot by the player
+	 * @param damageMultiplier how much to multiply the damage by
+	 * @param speedMultiplier how much to multiple the speed by
+	 */
 	public Projectile(Vector2 origin, float originRot, ProjectileData data,
 			boolean isPlayerProjectile, float damageMultiplier, float speedMultiplier) {
+		this.startingPos = origin.cpy();
 		position = origin;
-		speed = data.speed * speedMultiplier;
 		damage = data.damage * damageMultiplier;
 		this.isPlayerProjectile = isPlayerProjectile;
 
+		// speed is only needed to initialize the velocity
+		float speed = data.speed * speedMultiplier;
+
 		// Calculate the projectile's velocity in the game space
-		velocity = new Vector2((float) Math.cos(Math.toRadians(originRot)) * speed,
-				(float) Math.sin(Math.toRadians(originRot)) * speed);
+		velocity = new Vector2(speed, 0).setAngleDeg(originRot);
 
 		sprite = new Sprite(data.texture);
 		sprite.setSize(data.size.x, data.size.y);
@@ -62,14 +73,27 @@ public class Projectile extends PhysicsObject {
 		collisionPolygon = new Polygon(new float[] { data.size.x / 2, 0, data.size.x,
 				data.size.y / 2, data.size.x / 2, data.size.y, 0, data.size.y / 2 });
 		collisionPolygon.setOrigin(data.size.x / 2, data.size.y / 2);
+
+		lifeDist = MathUtils.random(500 - 50, 500 + 50);
+		lifeDist2 = lifeDist * lifeDist;
 	}
 
 	@Override
 	public void Update(float delta) {
 		position.x += velocity.x * delta;
 		position.y += velocity.y * delta;
-		collisionPolygon.setPosition(position.x - sprite.getWidth() / 2,
-				position.y - sprite.getHeight() / 2);
+		collisionPolygon.setPosition(position.x - getLocalCenterY(),
+				position.y - getLocalCenterX());
+
+		// test if projectile has passed the distance limit
+		if (startingPos.dst2(position) > lifeDist2) {
+			onTravelledMax();
+		}
+	}
+
+	/** Ran when the projectile has traveled beyond the maximum distance */
+	private void onTravelledMax() {
+		killOnNextTick = true;
 	}
 
 	@Override
@@ -80,12 +104,21 @@ public class Projectile extends PhysicsObject {
 
 	@Override
 	public void OnCollision(PhysicsObject other) {
-		if (other.getClass() == Projectile.class) {
+		if (other instanceof Projectile) {
 			Projectile p = (Projectile) other;
 			if (p.isPlayerProjectile != isPlayerProjectile) {
 				other.killOnNextTick = true;
 				killOnNextTick = true;
 			}
 		}
+	}
+
+	/**
+	 * Get the speed of the projectile
+	 * 
+	 * @return the speed
+	 */
+	public float getSpeed() {
+		return velocity.len();
 	}
 }
