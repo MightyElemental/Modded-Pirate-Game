2beeb773237af308c8aa7e194da546b24991ddce
James Burnell <94061765+uoy-jb2501@users.noreply.github.com>
2022-03-17 14:57:36 +0000

    Added test for projectile

---
+++ core/src/test/java/io/github/annabeths/Projectiles/ProjectileTest.java
@@ -0,0 +1,51 @@
+package io.github.annabeths.Projectiles;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.withSettings;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.math.Vector2;
+
+public class ProjectileTest {
+
+	@BeforeAll
+	public static void init() {
+		System.out.println("Setting up mock GL instance");
+		Gdx.gl = mock(GL20.class);
+	}
+
+	ProjectileData projTestData;
+
+	@BeforeEach
+	public void setupEach() {
+		System.out.println("Resetting projectile data");
+		projTestData = mock(ProjectileData.class,
+				withSettings().useConstructor(250f, 20f, new Vector2(20, 20), null));
+		when(projTestData.getDamage()).thenReturn(20f);
+		when(projTestData.getHeight()).thenReturn(20f);
+		when(projTestData.getWidth()).thenReturn(20f);
+		when(projTestData.getSpeed()).thenReturn(250f);
+	}
+
+	@Test
+	@DisplayName("Test projectile sets despawn flag after max travel distance")
+	public void testProjectileDespawn() {
+		Projectile p = new Projectile(Vector2.Zero, 0f, projTestData, true);
+
+		// update projectile to move it
+		for (int i = 0; i <= 4; i++) {
+			p.Update(0.5f);
+		}
+
+		assertTrue(p.killOnNextTick);
+	}
+
+}
