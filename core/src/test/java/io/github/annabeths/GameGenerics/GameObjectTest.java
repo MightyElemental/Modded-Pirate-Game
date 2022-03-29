fed57799c1b4873c08882841fd65b437d4914d13
James Burnell <94061765+uoy-jb2501@users.noreply.github.com>
2022-03-29 16:14:07 +0100

    Added tests for GameObject

--- core/src/test/java/io/github/annabeths/GameGenerics/GameObjectTest.java
+++ core/src/test/java/io/github/annabeths/GameGenerics/GameObjectTest.java
@@ -1,5 +1,6 @@
 package io.github.annabeths.GameGenerics;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
@@ -8,6 +9,22 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
 
 public class GameObjectTest {
+
+	@Test
+	public void testCenterNullSprite() {
+		GameObject go = new GameObject() {
+		};
+		assertDoesNotThrow(() -> go.getLocalCenterX());
+		assertDoesNotThrow(() -> go.getLocalCenterY());
+	}
+
+	@Test
+	public void testCenterNullPosition() {
+		GameObject go = new GameObject() {
+		};
+		assertDoesNotThrow(() -> go.getX());
+		assertDoesNotThrow(() -> go.getY());
+	}
 
 	@Test
 	public void testCenter() {
@@ -27,6 +44,39 @@ public class GameObjectTest {
 		go.sprite = new Sprite();
 		go.sprite.setBounds(100, 100, 100, 100);
 		assertEquals(go.getCenter(), new Vector2(150, 150));
+	}
+
+	@Test
+	public void testSetCenter() {
+		GameObject go = new GameObject() {
+		};
+		go.sprite = new Sprite();
+		go.sprite.setSize(50, 50);
+
+		go.setCenter(new Vector2(0, 0));
+		assertEquals(0, go.getCenterX());
+		assertEquals(0, go.getCenterY());
+		assertEquals(-25, go.getX());
+		assertEquals(-25, go.getY());
+
+		go.setCenter(new Vector2(100, 50));
+		assertEquals(100, go.getCenterX());
+		assertEquals(50, go.getCenterY());
+		assertEquals(75, go.getX());
+		assertEquals(25, go.getY());
+	}
+
+	@Test
+	public void testSetSprite() {
+		GameObject go = new GameObject() {
+		};
+		Vector2 size = new Vector2(100, 100);
+		Vector2 pos = new Vector2(50, 50);
+		go.setSprite("", pos, size);
+		assertEquals(size.x, go.sprite.getWidth());
+		assertEquals(size.y, go.sprite.getHeight());
+		assertEquals(pos.x, go.sprite.getX());
+		assertEquals(pos.y, go.sprite.getY());
 	}
 
 }
