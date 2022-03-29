cad756cd76f6c58c0f363abf03b51d9378efc805
James Burnell <94061765+uoy-jb2501@users.noreply.github.com>
2022-03-29 19:41:59 +0100

    Standardized code & removed unneeded condition

--- core/src/main/java/io/github/annabeths/Colleges/EnemyCollege.java
+++ core/src/main/java/io/github/annabeths/Colleges/EnemyCollege.java
@@ -59,14 +59,14 @@ public class EnemyCollege extends College {
 	@Override
 	public void OnCollision(PhysicsObject other) {
 		// if the enemy college is hit by a projectile
-		if (other instanceof Projectile && HP > 0) {
+		if (other instanceof Projectile && !isDead()) {
 			Projectile p = (Projectile) other;
 			if (p.isPlayerProjectile()) { // if its a player projectile
 				p.kill();
 				if (!isInvulnerable()) {
 					damage(p.getDamage());
 					updateHpText();
-					if (HP <= 0) gc.CollegeDestroyed(this);
+					if (isDead()) gc.CollegeDestroyed(this);
 				} else {
 					hpText.setText(font, "RESISTED, destroy other colleges first!");
 				}
@@ -75,7 +75,8 @@ public class EnemyCollege extends College {
 	}
 
 	public void Update(float delta) {
-		if (HP > 0) {
+		if (!isDead()) {
+			// increase the time on the timer to allow for fire rate calculation
 			timeSinceLastShot += delta;
 
 			PlayerBoat boat = gc.playerBoat;
@@ -95,12 +96,11 @@ public class EnemyCollege extends College {
 
 	public void Draw(SpriteBatch batch) {
 		islandSprite.draw(batch);
-		if (HP > 0) {
-			sprite.draw(batch);
-			font.draw(batch, hpText, getCenterX() - hpText.width / 2,
-					position.y - hpText.height / 2);
-		} else {
+		if (isDead()) {
 			deadSprite.draw(batch);
+		} else {
+			sprite.draw(batch);
+			font.draw(batch, hpText, getCenterX() - hpText.width / 2, getY() - hpText.height / 2);
 		}
 	}
 
